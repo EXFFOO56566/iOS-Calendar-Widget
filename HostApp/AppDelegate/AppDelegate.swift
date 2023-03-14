e852f80cff3948fe2ecd96bfcf16a001eb33264b
tochyodikwa <odikwatochy233@gmail.com>
2023-03-14 16:47:34 -0700

    First

---
+++ HostApp/AppDelegate/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  CalendarWidget
+//
+//  Created by Apps4World on 9/22/20.
+//
+
+import Apps4World
+
+/// This class will initialize/configure the `Apps4World` framework
+class AppDelegate {
+    static func configure() {
+        WidgetConfigurator.configure(purchaseCode: "CodeCanyon_Item_Purchase_Code")
+    }
+}
